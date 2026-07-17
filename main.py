f0a828e2e68083f2dc45e9176e472fb72bc7618d
Semyon Chistyakov <semencistckov@gmail.com>
2026-07-18 02:31:11 +0700

    new

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI,HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+
 app = FastAPI()
 
 
 balance = {}
 class OperationRequest(BaseModel):
-    wallet_name:str
+    wallet_name:str = Field(...,max_length=127)
     amount:float
-    description:str|None = None
+    description:str|None = Field(None,max_length=250)
+    @field_validator("amount")
+    @classmethod
+    def amount_not_negative(cls,v:float)->float:
+        if v<=0:
+            raise ValueError("")
+        return v
+
+    @field_validator("wallet_name")
+    def not_empty(cls,v:str)->str:
+        v = v.strip()
+        if not v:
+            raise ValueError("Name not fiend")
+        return v
 
 
 @app.get("/balance")
@@ -33,8 +47,6 @@ def create_wallet(name:str,initial_balance:float = 0):
 def add_income(operation:OperationRequest):
     if operation.wallet_name not in balance:
         raise HTTPException(status_code=400,detail = "wallet not found")
-    elif operation.amount<0:
-        raise HTTPException(status_code=400, detail="amount not be negative ")
     balance[operation.wallet_name]+=operation.amount
     return {
         "amount":operation.amount,
@@ -45,8 +57,8 @@ def add_income(operation:OperationRequest):
 def add_expense(operation:OperationRequest):
     if operation.wallet_name not in balance:
         raise HTTPException(status_code=400,detail = "wallet not found")
-    elif operation.amount<0:
-        raise HTTPException(status_code=400, detail="amount not be negative ")
+    if operation.amount>balance[operation.wallet_name]:
+        raise HTTPException(status_code=400,detail = "сумма больше чем есть на счете")
     balance[operation.wallet_name] -= operation.amount
     return {
         "amount":operation.amount,
